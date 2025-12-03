c6f5a7eeac2ecf9b481b02087f47dce1c0bc5d76
SUNGJUN94 <fiwns7942@gmail.com>
2025-12-03 10:10:58 +0900

    update

---
+++ Projects/ex26_module_numpy_pandas_matplotlib.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+aa=np.array([10,20,30])
+bb=np.array([4,5,6])
+
+print(aa)
+print(bb)
+cc= aa+bb
+print(cc)
+
+# 리스트와의 차이점 . 산수 덧셈을 하면 배열의 요소끼리 덧셈을 수행함
+
+print(aa.shape)
+print(cc.shape)
