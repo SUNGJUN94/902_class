f4176d673cd596735e5f16b4b08aa0ca95b3e270
SUNGJUN94 <fiwns7942@gmail.com>
2025-12-03 09:34:41 +0900

    update

--- Projects/ex24_module_web_scraping.py
+++ Projects/ex24_module_web_scraping.py
@@ -28,3 +28,31 @@ print(p_list[1].string)
 img= soup.select_one('#kk')
 print('이미지 경로 :', img.get('src')) #src 속성의 값을 얻어오기
 
+# 클래스로 찾기 
+es= soup.select('.aa')
+print(es[1].string)
+print()
+#--------------------------------------------------------------
+
+#[실습] 네이버 금융 페이지의 '코스피 지수' 스크래핑 해보기 (주의. 막 실행하면 안됨.)
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+response= requests.get('https://finance.naver.com/sise/', headers=headers)
+#print(response.text)
+soup= BeautifulSoup(response.text, 'html.parser')
+
+# 웹 문서에서 특정 요소를 찾기
+span_element= soup.select_one('#KOSPI_now')
+print('코스피 지수 :', span_element.string)
+
+
+
+#[과제] 웹 페이지 분석하기... [쇼핑몰 사이트의 가격 추출. robots.txt 확인]
+
+
+
+
