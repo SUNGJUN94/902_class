b33f344baf9a188ed9fc1fc3e3ee807dbe08836f
SUNGJUN94 <fiwns7942@gmail.com>
2025-12-04 10:18:29 +0900

    update

--- Projects/Hello.py
+++ Projects/Hello.py
@@ -1,4 +1,33 @@
-print(10)
-print('Hello world')
+# print(10)
+# print('Hello world')
 
-100
+# 100
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df= pd.read_excel('./files/team_score.xlsx')
+print(df.head())
+print(df['Unnamed: 0'])
+print(df['삼성'])
+
+plt.scatter(df['Unnamed: 0'], df['삼성'])
+#plt.boxplot([df['LG'], df['삼성']])
+plt.show()
+
+print(df.info())
+Q1= df['삼성'].quantile(0.25)
+Q3= df['삼성'].quantile(0.75)
+IRQ= Q3-Q1
+
+print(Q1)
+print(Q3)
+print(IRQ)
+
+aa= df[df['삼성'] > Q3+IRQ*1.5]
+print(aa)
+
+aa= df['삼성'] > Q3+IRQ*1.5
+print(aa)
+
